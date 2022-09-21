1c89fdb3b365e6d283a2218e64aa2880e100e6c0
nagarajasunder <gowthamvj15@gmail.com>
2022-09-21 06:53:44 +0530

    AWC-1

--- app/src/main/java/com/geekydroid/androidbookcompose/MainActivity.kt
+++ app/src/main/java/com/geekydroid/androidbookcompose/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     //LaunchedEffectDemo()
                     //RememberCoroutineScopeDemo()
                     //RememberUpdatedStateDemo()
-                    DisposableEffectDemo()
+                    //DisposableEffectDemo()
                 }
             }
         }
